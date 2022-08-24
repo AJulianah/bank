9182f2f8cb90c602511800dd8f558234c6d7837d
AJulianah <103264122+AJulianah@users.noreply.github.com>
2022-08-24 07:56:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ main(int argc, char const *argv[])
     char password[10];
 
     printf("\nQue veus-tu faire?");
-    printf("\n\n1. Creer un compte.");
+    printf("\n\n1. Creer un compte...");
     printf("\n\n2. Se connecter a un compte ");
 
     printf("\n\nEntrer votre choix: \t");
